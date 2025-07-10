d250df623949ff5a020b71f06f18078c9917a0ea
VARO707 <mod3site5@gmail.com>
2025-07-10 21:01:19 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const popup = document.getElementById("popUp");
+const photo = document.querySelectorAll(".PHOTO img");
+const poto = document.getElementById("popImg");
+const tutup = document.getElementById("closeBtn");
+
+photo.forEach((img) => {
+  img.addEventListener("click", () => {
+    poto.src = img.src;
+    popup.style.display = "flex";
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  popup.style.display = "none";
+});
+
+popup.addEventListener("click", (e) => {
+    if (e.target === popup) {
+  popup.style.display = "none";
+    }
+});
